8c3fe280e0c7577e358d0d18ccee7a48f3724a1c
FlashNight515 <2391748033@qq.com>
2020-04-11 19:35:20 +0800

    第一次提交，完成2个页面

---
+++ components/activityItem/activityItem.js
@@ -0,0 +1,30 @@
+// components/activityItem/activityItem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    activityTitle: { type: String, value:'文章标题文章标题文章标题文章标题文章标题文章标题文章标题'},
+    activityIssuer:{type:String,value:'软件工程党支部'},
+    activityDate:{type:Date,value:'2020-04-07'},
+    imageUrl:{type:String,value:'/images/滑动窗/1.jpg'}
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    //activity点击事件
+    activityClick:function(e){
+      console.log(e);
+    }
+
+  }
+})
---
+++ pages/activity/activity.js
@@ -0,0 +1,48 @@
+Page({
+
+  data: {
+    //单选框内容
+    xueyuanArray:['软件学院','计算机学院','信息科学学院','物理学院'],
+    xueyuanIndex:0,
+    typeArray:['实践活动','会议','奖惩公告','通知'],
+    typeIndex:0,
+    statusArray:['未开始','进行中','已结束'],
+    statusIndex:0,
+    orderArray:['时间','参加人数'],
+    orderIndex:0,
+    //活动内容
+    activityTitle:'入党积极份子大会',
+    activityIssuer:'党支部党委书记',
+    activityDate:'2020-5-1',
+    imageUrl:'/images/滑动窗/2.jpg',
+
+
+    
+  },
+
+  onLoad: function (options) {
+    
+  },
+
+  onReady: function () {
+    
+  },
+
+  onShow: function () {
+    
+  },
+
+  onHide: function () {
+    
+  },
+  onUnload: function () {
+  },
+  onPullDownRefresh: function () {
+  },
+
+  onReachBottom: function () {
+  },
+  onShareAppMessage: function () {
+    
+  }
+})
